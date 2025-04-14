b73838f4fe9170b72b31477ce0b9ff21180b3136
tethranialn <159460032+tethranialn@users.noreply.github.com>
2025-04-14 16:07:37 +0300

    not otchet

--- машам/FileProcessing.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-using namespace std;
-#include "List.h"
-#include <fstream>
-
-bool readMarker(ifstream& in, ofstream& out, List& list, const char marker);
-void processFile(const char* inputFile, const char* outputFile);
